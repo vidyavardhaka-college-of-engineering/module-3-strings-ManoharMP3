73307bb2b448ed48eb83ad67c377b42ef3117ed5
ManoharMP3 <manoharmp2703@gmail.com>
2021-03-26 10:31:55 +0000

    done

--- Program-1.c
+++ Program-1.c
@@ -1,4 +1,4 @@
-Complete the given program with string declaration/initialization & read/print the string
+//Complete the given program with string declaration/initialization & read/print the string
 // Instructions:-
 //1) Initially, declare a string with size
 //2) read the input string from user 
@@ -13,7 +13,7 @@ int main()
 {
 char a[100];                      //Declare the string
 printf("Enter a String: ");
-scanf("s",a);                      //read the string from user
-printf("The input string is %s",a);                      //Print the string
+scanf("%s",a);                      //read the string frouser
+printf("The input string is %s",a);     //Print the string
 return 0;
 }
