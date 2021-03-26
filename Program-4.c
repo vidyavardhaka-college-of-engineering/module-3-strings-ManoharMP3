e2b211781114a0e3c173474a84a8b007333da1ed
ManoharMP3 <manoharmp2703@gmail.com>
2021-03-26 11:12:18 +0000

    done

--- Program-4.c
+++ Program-4.c
@@ -16,7 +16,7 @@ int main()
     printf("Enter a string: ");
     scanf("%s",str);
     for(i=0; str[i]!='\0'; ++i);
-       printf("The length of string = );
-       printf("%d",i);//Complete the missing code in result printingstatement
+       printf("The length of string = %d",i);
+       //printf("%d",i);//Complete the missing code in result printingstatement
     return 0;
 }
